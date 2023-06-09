48eeb5da86a3343e163e2a2475bd4c206c07d3f9
Victor Golovin <rinoally@gmail.com>
2023-06-09 22:58:32 +1000

    add new ref file

--- js/expenses-app-after-ref-2.js
+++ js/expenses-app-after-ref-2.js
@@ -2,127 +2,109 @@
 
 
 // Объявление переменныйх - Строковых констант
-const LIMIT = 10000;
-const CURRENCY = 'руб.';
 const STATUS_IN_LIMIT = 'Все хорошо';
 const STATUS_OUT_OF_LIMIT = 'Все плохо';
-const STATUS_OUT_OF_LIMIT_CLASS_NAME = 'status-red';
+
 
 // Объявление переменныйх - ссылок на html элементы
 const inputNode = document.querySelector('.js-expense-input');
 const buttonNode = document.querySelector('.js-expense-button');
+const clearButtonNode = document.querySelector('.js-expense-clear-button');
+const totalValueNode = document.querySelector();
+const statusNode = document.querySelector('.js-status');
 const historyNode = document.querySelector('.js-history');
 const sumNode = document.querySelector('.js-sum');
+
+
+// Получаем лимит из элемента HTML с js-limit
 const limitNode = document.querySelector('.js-limit');
-const statusNode = document.querySelector('.js-status');
-const resetButtonNode = document.querySelector('.js-expense-reset-button');
+const limit = parseInt(limitNode.innerText);
+
 
 // Объявление нашей основной переменной 
 // При сапуске она содержит в себе пустой массив
 // Который мы пополняем по нажатию Добавить
-const expenses = [];
+let expenses = [];
 
 // ---ФУНКЦИИ------------------------------------------------------------
 
 
-initApp(expenses);
-
-
-
-buttonNode.addEventListener('click', function() {
-    const expense = getExpenseFromUser();
-    
-    if (!expense) {
-        return
-    }
-
-    trackExpanse(expense);
-
-    render(expenses);
-});
-
-
-resetButtonNode.addEventListener('click', function() {
-    renderHistory(expenses) === '';
-    calculateExpenses(expenses) ===''
-})
-
-// Полсчитывает и возвращает сумму всех трат
-function calculateExpenses(expenses) {
+// Подсчитываем и возвращаем сумму всех трат
+function getTotal() {
     let sum = 0;
-    expenses.forEach(element => {
-        // пробегаем по массиву обьектов expense
-        sum += element;
+    expenses.forEach(function(expense) {
+        // пробегаем по массиву обьектов expense, берем из каждого поле amount
+        // и прибавляем к переменной sum
+        sum += expense.amount;
     });
 
     return sum;
 }
 
-// Отрисовывает/обновляет блок с "Всего", "Лимит", "Статус"
-function renderStatus (sum) {
-    // 
-    if (sum <= LIMIT) {
+
+// Отрисовывает/обновляет блог с "Всего", "Лимит" и "Статус" 
+function renderStatus() {
+    // Создаем переменную total(всего) и записываем в нее результат выполнения getTotal
+    const total = getTotal(expenses);
+    totalValueNode.innerText = total;
+
+    // Условие сравнения - что больше всего или лимит
+    if (total <= limit) {
+        // всего меньше чем лимит - хорошо
         statusNode.innerText = STATUS_IN_LIMIT;
+        statusNode.className = "stats__statusText_positive";
     } else {
-        statusNode.innerText = STATUS_OUT_OF_LIMIT;
-        statusNode.classList.add(STATUS_OUT_OF_LIMIT_CLASS_NAME);
+        // всего меньше чем лимит - все плохо
+
+        // шаблонная строка - строка в которую можно вставить переменные
+        // тут вставлена переменная STATUS_OUT_OF_LIMIT
+        // и будет вставлено значение разницы между лимитом и общей суммой расходов
+        statusNode.innerText = `${STATUS_OUT_OF_LIMIT} (${limit - total} руб)`;
+        statusNode.className = "stats__statusText_negative";
     }
 }
 
+// Отрисовываем/обновляет блок
+function renderHistory() {
+    historyList.innerHTML = ""
+    // сокращаем запись:
+    // expenses.forEach((expense) => {})
 
-function initApp(expenses) {
-    limitNode.innerText = LIMIT;
-    sumNode.innerText = calculateExpenses(expenses);
-    statusNode.innerText = STATUS_IN_LIMIT;
+    // цикл по массиву expenses, где каждый элемент - запись в расходе (сумма и категория)
+    expenses.forEach(function (expense) {
+        // создание элемента li (он пока создан только в памяти)
+        const historyItem = document.createElement("li");
+
+        // через свойство className так же можно прописывать классы
+        historyItem.className = "rub";
+
+        // снова создаем шаблонную строку
+        // форма "категория" - "сумма" (а не наоборот, чтобы не усложнять html)
+
+        historyItem.innerText = `${expense.category} - ${expense.amount}`;
+
+        // берем нам li из памяти и вставляем в документ, в конце historyList
+        historyList.appendChild(historyItem);
+    })
 }
 
-// 2) Сохраняем трату в список
-function trackExpanse(expense) {
-    expenses.push(expense);
+// Отрисовываем/обновляем весь интерфейс (в нашем случае историю, всего, статус)
+function render() {
+    // вызываем функцию обновления статуса и "всего"
+    renderStatus();
+
+    // вызываем функцию обновления истории 
+    renderHistory();
 }
 
-
-// 1) Получаем значение из поля ввода
+// Возвращет введенную пользователем сумму
 function getExpenseFromUser() {
-    if (!inputNode.value) {
-        return null;
-    }
-
-    const expense = parseInt(inputNode.value);
- 
-    clearInput();
-
-    return expense;
+    return parseInt(inputNode.value);
 }
 
-function clearInput() {
-    inputNode.value = '';
-}
+// Возвращает выбранную пользователем категорию 
 
 
-
-function render(expenses) {
-    const sum = calculateExpenses(expenses);
-
-    renderHistory(expenses);
-    renderSum(sum);
-    renderStatus(sum);
-}
-
-// 3) Выведем новый список трат
-function renderHistory(expenses) {
-    let expensesListHTML = '';
-    expenses.forEach(element => {
-        const elementHTML = `<li>${element} ${CURRENCY}</li>`;
-        expensesListHTML += elementHTML;
-    });
-
-    historyNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
-}
-
-function renderSum(sum) {
-    sumNode.innerText = sum;
-}
 
 
 
